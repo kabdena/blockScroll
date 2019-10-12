178d29a6f8a6d2f048f20051b1d3392569de6578
Ayan <38213539+kabdena@users.noreply.github.com>
2019-10-12 15:02:53 +0600

    Add files via upload

---
+++ blockScroll.js
@@ -0,0 +1,23 @@
+// Scroll block function
+let windowOffsetTop = 0;
+
+function blockScroll( state ) {
+	let b = $('body');
+
+	if ( state === true ) {
+		windowOffsetTop = $(window).scrollTop();
+
+		b.css({
+			position: 'fixed',
+			top: -windowOffsetTop + 'px',
+			right: 0,
+			bottom: 0,
+			left: 0,
+			overflowY: 'scroll'
+		});
+	} else {
+		b.removeAttr('style');
+
+		window.scrollTo(0, windowOffsetTop);
+	}
+}
